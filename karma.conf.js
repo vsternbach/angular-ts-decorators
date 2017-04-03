9f94bc20e55e657681f13a6fd9976669f4f1d51e
sternbach <v.sternbach@f5.com>
2017-04-03 20:21:41 +0300

    changed tests to run on Chrome in local environment and custom Chrome in Travis CI environment

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,40 @@
 module.exports = function(config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', 'karma-typescript'],
-        files: [
-            "angular-ts-decorators.ts",
-            "spec/**/*.ts",
-        ],
-        exclude: ['src'],
-        preprocessors: {
-            'angular-ts-decorators.ts': ['coverage'],
-            '**/*.ts': ['karma-typescript'],
-        },
-        reporters: ['dots', 'coverage', 'karma-typescript'],
-        coverageReporter: {
-            dir: 'coverage',
-            type: 'html'
-        },
-        client: {
-            captureConsole: true
-        },
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['PhantomJS'],
-        singleRun: false,
-        concurrency: Infinity
-    });
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', 'karma-typescript'],
+    customLaunchers: {
+      chromeTravisCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    files: [
+      "angular-ts-decorators.ts",
+      "spec/**/*.ts",
+    ],
+    exclude: ['src'],
+    preprocessors: {
+      'angular-ts-decorators.ts': ['coverage'],
+      '**/*.ts': ['karma-typescript'],
+    },
+    reporters: ['dots', 'coverage', 'karma-typescript'],
+    coverageReporter: {
+      dir: 'coverage',
+      type: 'html'
+    },
+    client: {
+      captureConsole: true
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  });
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['chromeTravisCI'];
+  }
 };
