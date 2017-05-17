97f7fd527612c43fd68aad833e6d0b742a49ff97
sternbach <v.sternbach@f5.com>
2017-05-17 16:41:36 +0300

    fix(@NgModule): add backward compatibility for deprecated NgModuleDecorated

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { Directive } from './directive';
 export { Injectable } from './injectable';
 export { Pipe, PipeTransform } from './pipe';
 export { Input, Output } from './input';
-export { NgModule } from './module';
+export { NgModule, NgModule as /** @deprecated */ NgModuleDecorated } from './module';
 export { HostListener } from './hostListener';
 export * from './lifecycle_hooks';
