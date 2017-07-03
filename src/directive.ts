c478ded4973c829f9c8fdb9302f7b944f39aea42
Christopher Cortes <christophercr@gmail.com>
2017-07-03 16:21:16 +0200

    fix(@Directive): set controller option when no legacy compile/link functions are defined
    Fixes #31

--- src/directive.ts
+++ src/directive.ts
@@ -61,6 +61,7 @@ export function registerDirective(module: ng.IModule, ctrl: DirectiveControllerC
     directiveFunc.$inject = ['$injector', ...(ctrl.$inject || annotate(ctrl))];
   }
   else {
+    options.controller = ctrl;
     ctrl.$inject = ctrl.$inject || annotate(ctrl);
     const listeners: IHostListeners = getMetadata(metadataKeys.listeners, ctrl);
     if (listeners) {
