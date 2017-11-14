c1887db9dedc861620c6688dab195ffe28dc9efc
Erik Hughes <erik.hughes@outlook.com>
2017-11-14 16:15:38 +0100

    Allow filters/pipes to declare self as stateful
    This is to support legacy, albiet bad practice but sometimes needed stateful filters. Example of such a scenario is a translation pipes.

--- src/pipe.ts
+++ src/pipe.ts
@@ -11,16 +11,22 @@ export function Pipe(options: {name: string}) {
   return (Class: PipeTransformConstructor) => {
     defineMetadata(metadataKeys.name, options.name, Class);
     defineMetadata(metadataKeys.declaration, Declarations.pipe, Class);
+    defineMetadata(metadataKeys.options, options, Class);
   };
 }
 
 /** @internal */
 export function registerPipe(module: ng.IModule, filter: PipeTransformConstructor) {
   const name = getMetadata(metadataKeys.name, filter);
+  const options = getMetadata(metadataKeys.options, filter);
   const filterFactory = (...args: any[]) => {
     const injector = args[0]; // reference to $injector
     const instance = injector.instantiate(filter);
-    return instance.transform.bind(instance);
+    const transform = instance.transform.bind(instance);
+    if (options.stateful) {
+      transform.$stateful = options.stateful;
+    }
+    return transform;
   };
   filterFactory.$inject = ['$injector', ...(filter.$inject || annotate(filter))];
   module.filter(name, filterFactory);
