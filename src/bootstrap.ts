3920e0b8a605bb65b3bd462977efbe00acd3f726
sternbach <v.sternbach@f5.com>
2018-04-09 14:50:50 +0300

    fix(bootstrap): fix bug when compilerOptions are not provided

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -9,7 +9,7 @@ export interface CompilerOptions {
 export const platformBrowserDynamic = () => PlatformRef;
 
 export class PlatformRef {
-  static bootstrapModule(moduleType: Type<any>|string, compilerOptions?: CompilerOptions) {
+  static bootstrapModule(moduleType: Type<any>|string, compilerOptions: CompilerOptions = { strictDi: false }) {
     const moduleName = typeof moduleType === 'string' ? moduleType : (moduleType as NgModule).module.name;
     const strictDi = (compilerOptions.strictDi === true);
     element(document).ready(() => {
